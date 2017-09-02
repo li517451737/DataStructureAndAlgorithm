e2225f4da464bbbebf55bca19b7216845633fa25
孤鸿 夜 <litiansum@foxmail.com>
2017-09-02 17:58:25 +0800

    添加链表迭代器和测试代码

--- NodeList/CycleLinkedList.cs
+++ NodeList/CycleLinkedList.cs
@@ -12,16 +12,16 @@ namespace NodeList
     /// </summary>
     public class CycleLinkedList
     {
-        protected CycleNode current;
+        //protected CycleNode current;
 
         protected CycleNode header;
-        private int count;
+        //private int count;
 
         public CycleLinkedList()
         {
-            count = 0;
+            //count = 0;
             header = new CycleNode("header");
-            header.Link = header;
+            //header.Link = header;
         }
         /// <summary>
         /// 是否为空
@@ -38,7 +38,10 @@ namespace NodeList
         {
             header.Link = null;
         }
-
+        /**
+        /// <summary>
+        /// 遍历链表
+        /// </summary>
         public void PrintList()
         {
             CycleNode current = new CycleNode();
@@ -48,93 +51,207 @@ namespace NodeList
                 Console.WriteLine(current.Link.Element);
                 current = current.Link;
             }
-        }
-        /// <summary>
-        /// 查找指定元素的前一个节点
-        /// </summary>
-        /// <param name="n"></param>
-        /// <returns></returns>
-        public CycleNode FindPrevious(object n)
+        } 
+         */
+
+        public void ShowList()
         {
-            CycleNode current = header;
-            while (current.Link != null && current.Link.Element != n)
+            CycleNode current = header.Link;
+            while (current.Link!= null)
             {
+                Console.WriteLine(current.Element);
                 current = current.Link;
             }
-            return current;
         }
+        ///// <summary>
+        ///// 查找指定元素的前一个节点
+        ///// </summary>
+        ///// <param name="n"></param>
+        ///// <returns></returns>
+        //public CycleNode FindPrevious(object n)
+        //{
+        //    CycleNode current = header;
+        //    while (current.Link != null && current.Link.Element != n)
+        //    {
+        //        current = current.Link;
+        //    }
+        //    return current;
+        //}
         /// <summary>
         /// 查找指定元素所在节点
         /// </summary>
         /// <param name="n"></param>
         /// <returns></returns>
-        public CycleNode Find(object n)
-        {
-            CycleNode current = new CycleNode();
-            current = header.Link;
-            while (current.Element != n)
-            {
-                current = current.Link;
-            }
-            return current;
-        }
+        //public CycleNode Find(object n)
+        //{
+        //    CycleNode current = new CycleNode();
+        //    current = header.Link;
+        //    while (current.Element != n)
+        //    {
+        //        current = current.Link;
+        //    }
+        //    return current;
+        //}
+        ///// <summary>
+        ///// 移除指定元素的节点
+        ///// </summary>
+        ///// <param name="n"></param>
+        //public void Remove(object n)
+        //{
+        //    CycleNode current = FindPrevious(n);
+        //    if (current.Link != null)
+        //        current.Link = current.Link.Link;
+        //    count--;
+        //}
+        ///// <summary>
+        ///// 在指定节点后，查询新的元素节点
+        ///// </summary>
+        ///// <param name="n1">新节点元素</param>
+        ///// <param name="n2">指定节点元素</param>
+        //public void Insert(object n1, object n2)
+        //{
+        //    CycleNode current = new CycleNode();
+        //    CycleNode newNode = new CycleNode(n1);
+        //    current = Find(n2);
+        //    newNode.Link = current.Link;
+        //    current.Link = newNode;
+        //    count++;
+        //}
+        ///// <summary>
+        ///// 插入第一个元素
+        ///// </summary>
+        ///// <param name="n"></param>
+        //public void InsertFirst(object n)
+        //{
+        //    CycleNode current = new CycleNode(n);
+        //    current.Link = header;
+        //    header.Link = current;
+        //    count++;
+        //}
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <param name="n"></param>
+        ///// <returns></returns>
+        //public CycleNode Move(int n)
+        //{
+        //    CycleNode current = header.Link;
+        //    CycleNode temp;
+        //    for (int i = 0; i <= n; i++)
+        //    {
+        //        current = current.Link;
+        //    }
+        //    if (current.Element.ToString() == "header")
+        //    {
+        //        current = current.Link;
+        //    }
+        //    temp = current;
+        //    return temp;
+        //}
+       
         /// <summary>
-        /// 移除指定元素的节点
+        /// 获取第一个元素
         /// </summary>
-        /// <param name="n"></param>
-        public void Remove(object n)
-        {
-            CycleNode current = FindPrevious(n);
-            if (current.Link != null)
-                current.Link = current.Link.Link;
-            count--;
-        }
-        /// <summary>
-        /// 在指定节点后，查询新的元素节点
-        /// </summary>
-        /// <param name="n1">新节点元素</param>
-        /// <param name="n2">指定节点元素</param>
-        public void Insert(object n1, object n2)
-        {
-            CycleNode current = new CycleNode();
-            CycleNode newNode = new CycleNode(n1);
-            current = Find(n2);
-            newNode.Link = current.Link;
-            current.Link = newNode;
-            count++;
-        }
-        /// <summary>
-        /// 插入第一个元素
-        /// </summary>
-        /// <param name="n"></param>
-        public void InsertFirst(object n)
-        {
-            CycleNode current = new CycleNode(n);
-            current.Link = header;
-            header.Link = current;
-            count++;
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="n"></param>
         /// <returns></returns>
-        public CycleNode Move(int n)
+        public CycleNode GetFirst()
         {
-            CycleNode current = header.Link;
-            CycleNode temp;
-            for (int i = 0; i <= n; i++)
-            {
-                current = current.Link; 
-            }
-            if (current.Element.ToString() == "header")
-            {
-                current = current.Link;
-            } 
-            temp = current;
-            return temp;
+            return header;
         }
 
+    }
+    /// <summary>
+    /// 迭代器
+    /// </summary>
+    public class Listlter
+    {
+        /// <summary>
+        /// 当前节点
+        /// </summary>
+        private CycleNode current;
+        /// <summary>
+        /// 上一个节点
+        /// </summary>
+        private CycleNode previous;
+        /// <summary>
+        /// 链表实例
+        /// </summary>
+        CycleLinkedList theList;
+        public Listlter(CycleLinkedList list)
+        {
+            theList = list;
+            current = list.GetFirst();
+            previous = null;
+        }
+
+        /// <summary>
+        /// 在链表中从一个节点移动到下一个节点
+        /// </summary>
+        public void NextLink()
+        {
+            previous = current;
+            current = current.Link;
+        }
+        /// <summary>
+        /// 返回由迭代器指向的节点
+        /// </summary>
+        /// <returns></returns>
+        public CycleNode GetCurrent()
+        {
+            return current;
+        }
+
+        /// <summary>
+        /// 在当前节点钱插入指定元素
+        /// </summary>
+        /// <param name="element"></param>
+        public void InsertBefore(object element)
+        {
+            CycleNode newNode = new CycleNode(element);
+            if (current.Element.ToString() == "header")
+            {
+                throw new InsertBeforeHeaderException();
+            }
+            else
+            {
+                newNode.Link =previous.Link;
+                previous.Link = newNode;
+                current = newNode;
+            }
+        }
+        /// <summary>
+        /// 在当前节点后插入指定元素
+        /// </summary>
+        /// <param name="element"></param>
+        public void InsertAfter(object element)
+        {
+            CycleNode newNodel = new CycleNode(element);
+            newNodel.Link = current.Link;
+            current.Link = newNodel;
+            NextLink();
+        }
+        /// <summary>
+        /// 移除当前节点，将前节点的Link字段设置为当前节点的Link字段
+        /// </summary>
+        public void Remove()
+        {
+            previous.Link = current.Link;
+        }
+        /// <summary>
+        /// 重新设置为头节点
+        /// </summary>
+        public void Rest()
+        {
+            current = theList.GetFirst();
+            previous = null;
+        }
+        /// <summary>
+        /// 检查是否在表尾
+        /// </summary>
+        /// <returns></returns>
+        public bool AtEnd()
+        {
+            return current.Link == null;
+        }
     }
     /// <summary>
     /// 循环链表节点
@@ -155,6 +272,13 @@ namespace NodeList
         {
             Element = element;
             Link = null;
+        }
+    }
+    public class InsertBeforeHeaderException : Exception
+    {
+        public InsertBeforeHeaderException() : base("不能再头节点插入！")
+        {
+
         }
     }
 }
--- NodeList/Program.cs
+++ NodeList/Program.cs
@@ -10,6 +10,66 @@ namespace NodeList
     {
         static void Main(string[] args)
         {
+            CycleLinkedList MyList = new CycleLinkedList();
+            Listlter iter = new Listlter(MyList);
+            string choice, value;
+            iter.InsertAfter("David");
+            iter.InsertAfter("Mike");
+            iter.InsertAfter("Raymond");
+            iter.InsertAfter("Bernica");
+            iter.InsertAfter("Jennifer");
+            iter.InsertBefore("Donnie");
+            iter.InsertAfter("Michael");
+            iter.InsertBefore("Terrill");
+            iter.InsertBefore("Mayo");
+            iter.InsertBefore("Clayton");
+            while (true)
+            {
+                Console.WriteLine("(n) Move to next node");
+                Console.WriteLine("(g) Get value in current node");
+                Console.WriteLine("(r) Reset iterator");
+                Console.WriteLine("(s) Show complete list");
+                Console.WriteLine("(a) Insert after");
+                Console.WriteLine("(b) Insert before");
+                Console.WriteLine("(c) Clear the screen");
+                Console.WriteLine("(x) Exit");
+                Console.WriteLine();
+                Console.Write("Enter your choice: ");
+                choice = Console.ReadLine();
+                choice = choice.ToLower();
+                char[] onechar = choice.ToCharArray();
+                switch (onechar[0])
+                {
+                    case 'n':
+                        if (!MyList.IsEmpty() && !iter.AtEnd())
+                        {
+                            iter.NextLink();
+                        }
+                        else
+                        {
+                            Console.WriteLine("无法移动到下一个节点");
+                        }
+                        break;
+                    case 'g':
+                        if (!MyList.IsEmpty())
+                        {
+                            Console.WriteLine("当前元素为：" + iter.GetCurrent().Element);
+                        }
+                        else
+                        {
+                            Console.WriteLine("链表为空");
+                        }
+                        break;
+                    case 'r':
+                        iter.Rest();
+                        break;
+                    case 's':
+                        MyList.ShowList();
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
